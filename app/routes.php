28daaed59341d75060351e6ddb1aec6f2bfed1dc
ashajamal7 <axj552@gmail.com>
2025-07-09 18:16:45 +0300

    Add files via upload

--- app/routes.php
+++ app/routes.php
@@ -6,6 +6,9 @@ $router = new App\Core\Router();
 // Define routes
 $router->get('/', 'HomeController@index');
 $router->get('/home', 'HomeController@index');
+$router->get('/products', 'ProductController@index');
+$router->get('/products/{:id}', 'ProductController@show');
+$router->get('/cart', 'CartController@index');
 
 // 404 Not Found handler
 $router->setNotFoundHandler('HomeController@notFound');
